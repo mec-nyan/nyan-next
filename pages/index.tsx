54bf841649c8774e401c1e49173cc5df4d830050
mec-nyan <marianoraspberrypi@gmail.com>
2022-03-17 14:04:16 -0300

    es en

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const [msg, setMsg] = useState("default msg");
+
+  useEffect(() => {
+    console.log("render");
+    if (router.query?.hl === "es") setMsg("Hola");
+    if (router.query?.hl === "en") setMsg("Hello");
+  }, [router.query]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,9 +27,10 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h2>{msg}</h2>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
 
@@ -59,14 +71,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
